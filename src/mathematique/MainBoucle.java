0766068ef0412d1b00e359aaa89e6bd7bc913857
khalil <khalillakhdharatc@gmail.com>
2023-03-08 12:56:32 +0100

    for

---
+++ src/mathematique/MainBoucle.java
@@ -0,0 +1,15 @@
+package mathematique;
+
+public class MainBoucle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// x=4 y=20 [4,20]
+		for(int i=4;i<=20;i++)
+		{
+			if(i%2==0)
+				System.out.println("i="+i);
+		}
+	}
+
+}
