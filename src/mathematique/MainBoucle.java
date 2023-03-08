3a6b3dba27976d1b3527e91e25073450c41b889b
khalil <khalillakhdharatc@gmail.com>
2023-03-08 15:30:51 +0100

    boucles

--- src/mathematique/MainBoucle.java
+++ src/mathematique/MainBoucle.java
@@ -6,9 +6,17 @@ public class MainBoucle {
 		// TODO Auto-generated method stub
 		// x=4 y=20 [4,20]
 		for(int i=4;i<=20;i++)
+		//For (initialisation;limite;pas)
 		{
-			if(i%2==0)
-				System.out.println("i="+i);
+			//if(i%2==0)
+			//	System.out.println("i="+i);
+		}
+		int x=4;
+		while(x>=0)
+		{
+			System.out.print("x="+x);
+			x--;
+			// x=x-1;
 		}
 	}
 
