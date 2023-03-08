90f13a763b8b2c320467a92e004bda592edfad6d
khalil <khalillakhdharatc@gmail.com>
2023-03-08 11:02:30 +0100

    Initial commit

---
+++ src/imc/Personne.java
@@ -0,0 +1,58 @@
+package imc;
+
+public class Personne {
+	//boilerplate code
+private String nomComplet;
+private double poids,taille;
+
+
+
+public Personne() {
+	super();
+}
+public Personne(String nomComplet, double poids, double taille) {
+	super();
+	this.nomComplet = nomComplet;
+	this.poids = poids;
+	this.taille = taille;
+}
+//encapsulation
+public String getNomComplet() {
+	return nomComplet;
+}
+public void setNomComplet(String nomComplet) {
+	this.nomComplet = nomComplet;
+}
+public double getPoids() {
+	return poids;
+}
+public void setPoids(double poids) {
+	this.poids = poids;
+}
+public double getTaille() {
+	return taille;
+}
+public void setTaille(double taille) {
+	this.taille = taille;
+}
+public long indice()
+{
+double imc=this.poids/(Math.pow(this.taille,2));
+return Math.round(imc); // l'arrondie de IMC
+}
+public String evaluation()
+{
+long valeurImc=this.indice();
+if(valeurImc<20)
+	return "vous êtes maigre";
+else if(valeurImc<=25)
+{
+	return "vous êtes idéale!";
+}
+else
+	return "vous êtes en surpoid";
+	
+
+}
+
+}
---
+++ src/imc/Test.java
@@ -0,0 +1,13 @@
+package imc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Personne p1=new Personne();
+		p1.setNomComplet("Ali");
+		System.out.println(p1.getNomComplet());
+		Personne p2=new Personne("User ", 80, 1.90);
+	}
+
+}
